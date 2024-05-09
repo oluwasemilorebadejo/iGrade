7d1607cec4db3100ec98e93feb41622340f964d5
Victor Momodu <victoramomodu@gmail.com>
2024-05-10 00:08:59 +0100

    feat: Add favicon, logo and download button

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const departments = [
 ];
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
